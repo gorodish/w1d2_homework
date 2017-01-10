f116bd99f5b903cb4fe75d2605ef6f6753ff2164
Murray McQuarrie <murray.mcquarrie@gmail.com>
2017-01-10 15:40:14 +0000

    add month short function

---
+++ month_short.rb
@@ -0,0 +1,23 @@
+def month_short(month)
+
+  case month
+  when 1
+    puts "Jan"
+  when 2
+    puts "Feb"
+  when 3
+    puts "Mar"
+  when 4
+    puts "Apr"
+  when 5
+    puts "May"
+  when 6
+    puts "Jun"
+  when 7
+    puts "Jul"
+  when 8
+    puts "Aug"
+  end
+end
+
+puts month_short(8)
