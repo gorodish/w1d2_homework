d1bc103dc4a6c860468fa3ac83789f4cb1550792
Murray McQuarrie <murray.mcquarrie@gmail.com>
2017-01-10 15:32:16 +0000

    add month function

---
+++ month.rb
@@ -0,0 +1,23 @@
+def month_calc(month)
+  
+  case month
+  when 1
+    puts "January"
+  when 2
+    puts "February"
+  when 3
+    puts "March"
+  when 4
+    puts "April"
+  when 5
+    puts "May"
+  when 6
+    puts "June"
+  when 7
+    puts "July"
+  when 8
+    puts "August"
+  end
+end
+
+puts month_calc(8)
